0f6ac90edde957e134e6c78c18d97382f5be8d6c
jominime <jominime14@gmail.com>
2021-03-24 17:25:46 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input />
+      <button>Add a task</button>
+    </div>
+  );
+}
+
+export default App;
